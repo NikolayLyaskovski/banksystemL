8dee8f5cbfa7bcd52aa66ca72adac62c5ba5752a
Nikolay:yaskovski <nrlyaskovski@gmail.com>
2020-06-12 21:35:22 +0300

    make few new things getter and setters and first attempt for use Postman

--- bank.system/src/main/java/com/advance/bank/bank/system/controller/UserController.java
+++ bank.system/src/main/java/com/advance/bank/bank/system/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.advance.bank.bank.system.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.advance.bank.bank.system.model.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("api/users")
 public class UserController {
+
+
+    @PostMapping
+    public String createUser(@RequestBody User user){
+        return "Hello ".concat(user.getFirstName());
+    }
+
 
     @GetMapping("/test")
     private String test(){
