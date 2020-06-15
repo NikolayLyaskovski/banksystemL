d126c286494eadf5585d0e63e5d90db72af8ab0a
Nikolay:yaskovski <nrlyaskovski@gmail.com>
2020-06-15 19:26:10 +0300

    making interfaces and making classes services(services is the place for all the logic)

--- bank.system/src/main/java/com/advance/bank/bank/system/service/UserService.java
+++ bank.system/src/main/java/com/advance/bank/bank/system/service/UserService.java
@@ -1,5 +1,15 @@
 package com.advance.bank.bank.system.service;
 
-public interface UserService {
+import com.advance.bank.bank.system.model.User;
 
+import java.util.List;
+
+public interface UserService {
+   void createUser(User user);      //metodite v interfeisa sa abstractni i nqmat tqlo
+   void updateUser(User user);
+
+   User getUserById(Long id);
+   List<User> getAllUser();
+
+   void deleteUserById (Long id);
 }
---
+++ bank.system/src/main/java/com/advance/bank/bank/system/service/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.advance.bank.bank.system.service;
+
+import com.advance.bank.bank.system.model.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public void createUser(User user) {
+
+    }
+
+    @Override
+    public void updateUser(User user) {
+
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return null;
+    }
+
+    @Override
+    public void deleteUserById(Long id) {
+
+    }
+}
